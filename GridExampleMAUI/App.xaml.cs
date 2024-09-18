cbd0394a1157a3c84015cd4f99953a44d53ffd4d
chajus1 <chajus1@googlemail.com>
2024-09-19 00:01:40 +0100

    Add project files.

---
+++ GridExampleMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GridExampleMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GridExampleMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GridExampleMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
